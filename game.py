56c831c4676c8dcbd670af51e89921776fbe74fe
vPhlex <115638660+vPhlex@users.noreply.github.com>
2022-10-17 11:07:52 -0400

    starting process

--- game.py
+++ game.py
@@ -1,6 +1,13 @@
+import sys
+
 import pygame
 
+class AlienInvasion:
+    """Overall class to manage game assets and behavior."""
 
-print("welcome to pygame!")
+    def_init_(self):
+    """Initialize the game, and create game resources."""
+    pygame.init()
 
-print("Hello World")
+    self.screen = pygame.display.set_mode((1200,800))
+    pygame.display.set_caption("Alien Invasion")
