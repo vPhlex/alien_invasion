0eff72b48983941ef31047977bb8deefc50e76d5
vPhlex <115638660+vPhlex@users.noreply.github.com>
2022-10-20 09:48:07 -0400

    First part of alien game

---
+++ Homework/Blue_Sky.py
@@ -0,0 +1,9 @@
+import pygame
+import time
+
+pygame.init()
+screen = pygame.display.set_mode((200, 200))
+screen.fill((0, 0, 255))
+pygame.display.flip()
+
+time.sleep(8)
--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 import sys
-
 import pygame
+
+from settings import Settings
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -8,6 +9,33 @@ class AlienInvasion:
     def_init_(self):
     """Initialize the game, and create game resources."""
     pygame.init()
+    self.settings = Settings()
 
-    self.screen = pygame.display.set_mode((1200,800))
+    self.screen = pygame.display.set_mode(
+        (self.settings.screen_width, self.settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+
+    self.ship = Ship(self)
+
+    #set the background color.
+    self.bg_color = (230, 230, 230)
+
+def run_game(self):
+    """Start the main loop for the game."""
+    while True:
+        # Watch for keyboard and mouse events.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        #Redraw the screen during each pass through the loop.
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+            # Make the most recently drawn screen visible.
+            pygame.display.flip()
+
+if_name_ == '_main_':
+    # Make a game instance, and run the game.
+    ai = AlienInvasion()
+    ai.run_game()
